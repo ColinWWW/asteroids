db49fa33e9f065c3a6a1673a2c51488507ff4761
ColinWWW <colin.wallace001@gmail.com>
2025-02-04 15:02:10 -0500

    Asteroids move and i can shoot and move

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
-
+from shot import Shot
 
 def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    Player.containers = (updatable_group, drawable_group)
     player = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
+    asteroids = pygame.sprite.Group()
+    AsteroidField.containers = (updatable_group,)
+    AsteroidField()
+    Asteroid.containers = (asteroids,updatable_group,drawable_group)
+    Shot.containers = (updatable_group, drawable_group)
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                return
 
+
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        updatable_group.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game Over")
+                pygame.QUIT
+                return
+        for drawable in drawable_group:
+            print(f"Drawing: {type(drawable)}")
+            drawable.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-
 
 if __name__ == "__main__":
     main()
